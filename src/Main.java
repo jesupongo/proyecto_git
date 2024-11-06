6baf509e5b56744e441018b01ff5c8a8abb15449
Jesu <jgonlec2206@g.educaand.es>
2024-11-06 09:34:22 +0100

    Cambios en nueva rama

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hola Mundo!");
-        System.out.println("Esta es la nueva funcionalidad!");
+        System.out.println("Esta es la nueva funcionalidad!!");
     }
 }
